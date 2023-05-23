2cf10ee100a345b22fee77708ea026d0c9541d38
TCA <tomek.chady@gmail.com>
2023-05-23 21:36:48 +0200

    Removed unnecessary -lz from compile

--- compile.sh
+++ compile.sh
@@ -10,4 +10,4 @@ gcc chunkParser.c -o chunkParser.o $1 -c -lm #our chunk file handling lib
 gcc model.c -o model.o $1 -c #our 3d model handling lib
 gcc regionFileReader.c regionParser.o -o regionFileReader -lz $1 #utility 1
 gcc chunkExtractor.c regionParser.o -o chunkExtractor -lz -lm $1 #utility 2
-gcc modelGenerator.c model.o chunkParser.o nbt_parsing.o nbt_treeops.o buffer.o nbt_util.o -o modelGenerator -lz -lm $1 # utility 3
+gcc modelGenerator.c model.o chunkParser.o nbt_parsing.o nbt_treeops.o buffer.o nbt_util.o -o modelGenerator -lm $1 # utility 3
--- winCompile.sh
+++ winCompile.sh
@@ -6,4 +6,4 @@ x86_64-w64-mingw32-gcc-win32  chunkParser.c -o chunkParser.o -c -lm
 x86_64-w64-mingw32-gcc-win32  model.c -o model.o -c
 x86_64-w64-mingw32-gcc-win32  regionFileReader.c regionParser.o -o regionFileReader.exe -lz
 x86_64-w64-mingw32-gcc-win32  chunkExtractor.c regionParser.o -o chunkExtractor.exe -lz -lm
-x86_64-w64-mingw32-gcc-win32  modelGenerator.c model.o chunkParser.o nbt_parsing.o nbt_treeops.o buffer.o nbt_util.o -o modelGenerator -lz -lm
+x86_64-w64-mingw32-gcc-win32  modelGenerator.c model.o chunkParser.o nbt_parsing.o nbt_treeops.o buffer.o nbt_util.o -o modelGenerator -lm
