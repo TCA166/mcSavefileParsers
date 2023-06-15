97879ae7eb1cb82a0a486b7729c1293dd812a5e1
TCA <tomek.chady@gmail.com>
2023-06-15 20:26:17 +0200

    Fixed mtlGen The transparency of color corrected was set wrong

--- mtlGen.py
+++ mtlGen.py
@@ -22,27 +22,24 @@ if __name__ == "__main__":
             #simplified functioning that doesn't require textures
             if os.path.isfile(f) and filename[-4:] == ".png" and s:
                 rgb = [0, 0, 0, 0]
+                img = Image.open(f).convert('RGBA')
+                for x in range(img.size[0]):
+                    for y in range(img.size[1]):
+                        pixel = img.getpixel((x, y))
+                        rgb[0] += pixel[0]
+                        rgb[1] += pixel[1]
+                        rgb[2] += pixel[2]
+                        rgb[3] += pixel[3]
+                amt = img.size[0] * img.size[1]
+                rgb[0] /= (amt * 255)
+                rgb[1] /= (amt * 255)
+                rgb[2] /= (amt * 255)
+                rgb[3] /= (amt * 255)
                 if "grass" in filename or "leaves" in filename:
                     # #8eb971
                     rgb[0] = 142 / 255
                     rgb[1] = 185 / 255
                     rgb[2] = 113 / 255
-                    rgb[3] = 1
-                else:
-                    img = Image.open(f).convert('RGBA')
-                    for x in range(img.size[0]):
-                        for y in range(img.size[1]):
-                            pixel = img.getpixel((x, y))
-                            rgb[0] += pixel[0]
-                            rgb[1] += pixel[1]
-                            rgb[2] += pixel[2]
-                            rgb[3] += pixel[3]
-                    amt = img.size[0] * img.size[1]
-                    
-                    rgb[0] /= (amt * 255)
-                    rgb[1] /= (amt * 255)
-                    rgb[2] /= (amt * 255)
-                    rgb[3] /= (amt * 255)
                 if "top" in filename and t:
                     filename = filename.replace("_top", "", 1)
                 o.write("newmtl %s\n" % filename[:-4])
